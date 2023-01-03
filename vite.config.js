c3795f98684d73d04d66798c7c9d119e9346220e
Tapan <tapan@topsinfosolutions.com>
2023-01-03 16:37:50 +0530

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         vue({
